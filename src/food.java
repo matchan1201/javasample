110652a7a490b31934cc889047bd593406ca3a30
赤松由浩 <58959018+matchan1201@users.noreply.github.com>
2022-06-03 18:30:38 +0900

    Create food.java

---
+++ src/food.java
@@ -0,0 +1,17 @@
+public class food {
+  private String genre;  enumでいいかも？
+  private int calorie;
+  private int price;
+  private String shopName;
+  
+  public food(String genre, int calorie, int price, String shopName) {
+    this.genre = genre;
+    this.calorie = calorie;
+    this.price = price;
+    this.shopName = shopName;
+  } 
+  
+  インターフェースにして作り出した方がいいの？
+    それともクラスにして作り出した方がいいの？？
+  
+}
